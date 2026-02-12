2a5ef3c968af39d6446533fb0283a622faf9f59f
anukalpk50-stack <anukalpk50@gmail.com>
2026-02-12 12:34:34 +0530

    matrix comlited

--- matrix text/main.js
+++ matrix text/main.js
@@ -1,14 +1,19 @@
+
 const para = document.querySelector("p");
 const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-const text = para.innerText
-para.addEventListener("mouseenter", () => {
-  setInterval(() => {
-      const str = text.split("").map((char, index) => {
-        return characters.split("")[Math.floor(Math.random() * 53)]
-    }).join("")
-    
-   para.innerText = str
+const text = para.innerText;
 
-  },30)
-
-})
+let iteration = 0;
+function randomText(){
+  const str = text.split("").map((char , index) =>{
+    if (index < iteration){
+      return char
+    }
+    return characters.split("")[Math.floor(Math.random() *53)]
+  }).join("")
+  console.log(str)
+  para.innerText = str;
+  iteration += 0.15;
+  console.log(iteration)
+}
+setInterval(randomText,50)
